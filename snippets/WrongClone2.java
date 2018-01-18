8a365a1dbc65654991cdfa0df622ddb8085400aa
Daniel Perez <tuvistavie@gmail.com>
2018-01-18 16:43:11 +0900

    Add comparison with existing methods

---
+++ snippets/WrongClone2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args){
+    Scanner read = new Scanner(System.in);
+    int x = Integer.parseInt(read.nextLine());
+    int c = 0;
+    int sum = 0;
+    while (sum < x){
+      c++;
+      sum += c;
+    }
+    System.out.println(c);
+  }
+}
